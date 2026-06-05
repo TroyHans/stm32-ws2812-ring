521a26864ad0c2ed240d8d93f35142da4025f294
Troy <75467422+TroyHans@users.noreply.github.com>
2026-06-05 14:41:09 -0400

    Update main.c  for consistency

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -6,16 +6,13 @@
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * Copyright (c) 2026 Troy Hans
+  * This project is licensed under the MIT License.
   *
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "dma.h"
@@ -26,57 +23,51 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define MAX_LED 16
-#define USE_BRIGHTNESS 1
+#define MAX_LED         16
+#define USE_BRIGHTNESS  1
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-
 /* USER CODE BEGIN PV */
-/********** WS2812 Variables ***********************/
-uint8_t LED_Data[MAX_LED][4];
-uint8_t LED_Mod[MAX_LED][4];   // for brightness
-uint8_t datasentflag = 0;
+/* ==================== WS2812 Variables ==================== */
+uint8_t  LED_Data[MAX_LED][4];
+uint8_t  LED_Mod[MAX_LED][4];      // Working copy for brightness
+uint8_t  datasentflag = 0;
 uint32_t pwmData[(24u * MAX_LED) + 50];
-/********** END WS2812 Variables *******************/
-
-/*****************Display Variables************************* */
-
-/*******************END Display Variables********************** */
+/* ==================== End WS2812 Variables ==================== */
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+
 /* USER CODE BEGIN PFP */
-/* WS2812 Function Prototypes */
+/* ==================== WS2812 Function Prototypes ==================== */
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim);
 void WS2812_Send(void);
 void Set_LED(uint32_t LEDnum, uint8_t Red, uint8_t Green, uint8_t Blue);
 void Set_Brightness(uint8_t brightness);
+/* ==================== End WS2812 Function Prototypes ==================== */
 
-/* Animation Function Prototypes */
+/* ==================== Animation Function Prototypes ==================== */
 void Set_Rainbow(void);
 void Set_Rotating_Rainbow(void);
+/* ==================== End Animation Function Prototypes ==================== */
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
 /* USER CODE END 0 */
 
 /**
@@ -85,25 +76,19 @@ void Set_Rotating_Rainbow(void);
   */
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
-
   /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
@@ -111,12 +96,10 @@ int main(void)
   MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_TIM1_Init();
-/* USER CODE BEGIN 2 */
-  // Create a nice rainbow across all 16 LEDs
-  Set_Rainbow();
-  // Set a fixed brightness level once (0 to 45)
-  //Set_Brightness(30);   // Change this number to adjust brightness (30-40 recommended)
-/* USER CODE END 2 */
+
+  /* USER CODE BEGIN 2 */
+  Set_Rainbow();                    // Initial rainbow pattern
+  /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
@@ -128,9 +111,9 @@ int main(void)
     Set_Rotating_Rainbow();
     Set_Brightness(45);
     WS2812_Send();
-    HAL_Delay(30);          // Fast refresh rate for smooth animation
+    HAL_Delay(30);                  // Adjust for rotation speed
+    /* USER CODE END 3 */
   }
-  /* USER CODE END 3 */
 }
 
 /**
@@ -142,14 +125,9 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -158,13 +136,12 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 72;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
+
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -180,21 +157,17 @@ void SystemClock_Config(void)
 
 /* USER CODE BEGIN 4 */
 
-/*-----------------Display functions------------------------------------*/
+/* ==================== Animation Functions ==================== */
 
-/***************************Static Rainbow****************************** */
 void Set_Rainbow(void)
 {
     for(uint32_t i = 0; i < MAX_LED; i++)
     {
         uint8_t r, g, b;
-        
-        // Calculate hue (0 to 359)
         uint32_t hue = (i * 360u) / MAX_LED;
 
         uint8_t sector = hue / 60u;
         uint8_t remainder = hue % 60u;
-        
         uint8_t q = (255u * (60u - remainder)) / 60u;
         uint8_t t = (255u * remainder) / 60u;
 
@@ -212,16 +185,13 @@ void Set_Rainbow(void)
     }
 }
 
-/************************END Static Rainbow******************************************** */
-
-/****************************Rotating Rainbow***************************************** */
 void Set_Rotating_Rainbow(void)
 {
     static uint8_t offset = 0;
 
     for(uint32_t i = 0; i < MAX_LED; i++)
     {
-        uint8_t pos = (i * 16 + offset) % 256;   // 256-step color wheel for smoothness
+        uint8_t pos = (uint8_t)((i * 16u + offset) % 256u);
         uint8_t r, g, b;
 
         if(pos < 85)
@@ -248,87 +218,80 @@ void Set_Rotating_Rainbow(void)
         Set_LED(i, r, g, b);
     }
 
-    offset += 1;        // ← Change this number to control speed (2=slow, 8=fast)
+    offset += 1;        // Change this value to adjust rotation speed
 }
-/***************************END Rotating Rainbow****************************************** */
-/*---------------------------END Display Functions------------------------------------------*/
 
-/************************WS2812 Functions*************************************************** */
-void Set_LED (uint32_t LEDnum, uint8_t Red, uint8_t Green, uint8_t Blue)
+/* ==================== WS2812 Driver Functions ==================== */
+
+void Set_LED(uint32_t LEDnum, uint8_t Red, uint8_t Green, uint8_t Blue)
 {
-	LED_Data[LEDnum][0] = LEDnum;
-	LED_Data[LEDnum][1] = Green;
-	LED_Data[LEDnum][2] = Red;
-	LED_Data[LEDnum][3] = Blue;
+    LED_Data[LEDnum][0] = (uint8_t)LEDnum;
+    LED_Data[LEDnum][1] = Green;
+    LED_Data[LEDnum][2] = Red;
+    LED_Data[LEDnum][3] = Blue;
 }
 
-
-void Set_Brightness (uint8_t brightness)  // brightness: 0 to 45
+void Set_Brightness(uint8_t brightness)
 {
 #if USE_BRIGHTNESS
-
     if (brightness > 45) brightness = 45;
     if (brightness < 0)  brightness = 0;
 
-    float scale = brightness / 45.0f;   // 0.0 to 1.0
+    float scale = brightness / 45.0f;
 
-    for (uint32_t i = 0; i < MAX_LED; i++)
+    for(uint32_t i = 0; i < MAX_LED; i++)
     {
-        LED_Mod[i][0] = LED_Data[i][0];           // LED number (unused)
-        LED_Mod[i][1] = LED_Data[i][1] * scale;   // Green
-        LED_Mod[i][2] = LED_Data[i][2] * scale;   // Red
-        LED_Mod[i][3] = LED_Data[i][3] * scale;   // Blue
+        LED_Mod[i][0] = LED_Data[i][0];
+        LED_Mod[i][1] = (uint8_t)(LED_Data[i][1] * scale);
+        LED_Mod[i][2] = (uint8_t)(LED_Data[i][2] * scale);
+        LED_Mod[i][3] = (uint8_t)(LED_Data[i][3] * scale);
     }
-
 #endif
 }
+
 void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
 {
-	HAL_TIM_PWM_Stop_DMA(htim, TIM_CHANNEL_1);
-	datasentflag = 1;
+    HAL_TIM_PWM_Stop_DMA(htim, TIM_CHANNEL_1);
+    datasentflag = 1;
 }
 
-
-void WS2812_Send (void)
+void WS2812_Send(void)
 {
-	uint32_t indx=0;
-	uint32_t color;
+    uint32_t indx = 0;
+    uint32_t color;
 
-
-	for (uint32_t i= 0; i<MAX_LED; i++)
-	{
+    for(uint32_t i = 0; i < MAX_LED; i++)
+    {
 #if USE_BRIGHTNESS
-		color = ((LED_Mod[i][1]<<16) | (LED_Mod[i][2]<<8) | (LED_Mod[i][3]));
+        color = ((LED_Mod[i][1] << 16) | (LED_Mod[i][2] << 8) | LED_Mod[i][3]);
 #else
-		color = ((LED_Data[i][1]<<16) | (LED_Data[i][2]<<8) | (LED_Data[i][3]));
+        color = ((LED_Data[i][1] << 16) | (LED_Data[i][2] << 8) | LED_Data[i][3]);
 #endif
 
-		for (uint8_t i=23; i>=0; i--)
-		{
-			if (color&(1<<i))
-			{
-				pwmData[indx] = 60;  // 2/3 of 90
-			}
+        for(int8_t bit = 23; bit >= 0; bit--)
+        {
+            if (color & (1u << bit))
+                pwmData[indx] = 60;   // Logic 1
+            else
+                pwmData[indx] = 30;   // Logic 0
 
-			else pwmData[indx] = 30;  // 1/3 of 90
+            indx++;
+        }
+    }
 
-			indx++;
-		}
+    /* Reset pulse */
+    for(uint8_t i = 0; i < 50; i++)
+    {
+        pwmData[indx++] = 0;
+    }
 
-	}
+    HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, indx);
 
-	for (uint8_t i=0; i<50; i++)
-	{
-		pwmData[indx] = 0;
-		indx++;
-	}
-
-	HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pwmData, indx);
-	while (!datasentflag){};
-	datasentflag = 0;
+    while(!datasentflag) {}
+    datasentflag = 0;
 }
-/*************************END WS2812 Functions*********************************************** */
 
+/* ==================== End WS2812 Driver Functions ==================== */
 
 /* USER CODE END 4 */
 
@@ -338,27 +301,14 @@ void WS2812_Send (void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
+
 #ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
